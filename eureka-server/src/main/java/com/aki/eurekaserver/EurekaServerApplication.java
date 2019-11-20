7708ae73cf53659f35dd99df0537822055e51b11
kamiaki <247557768@qq.com>
2019-11-20 18:01:20 +0800

    更新一下

--- eureka-server/src/main/java/com/aki/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/aki/eurekaserver/EurekaServerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 这是springcloud的消息中心
+ * 访问控制台地址：http://localhost:8761/
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
