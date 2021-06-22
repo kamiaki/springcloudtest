a0929c8c8f11ff2ffcd0e50190796e8ee408bcf9
kamiaki <247557768@qq.com>
2021-06-22 17:02:18 +0800

    加入配置中心

--- config-client/src/main/java/com/aki/configclient/ConfigController.java
+++ config-client/src/main/java/com/aki/configclient/ConfigController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 访问地址 http://localhost:8880/port
+ */
 @RestController
 public class ConfigController {
     @Value("${server.port}")
